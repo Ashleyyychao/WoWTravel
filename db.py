fd23f3be57133e02082e28375d7ccd98f71040e7
FinallyPeace <ll7856l@gmail.com>
2021-05-01 03:52:19 +0800

    新增 註冊表單

--- db.py
+++ db.py
@@ -4,10 +4,14 @@ print('Opened database successfully')
 
 con.execute(
     """
-    CREATE TABLE account (
+    CREATE TABLE member (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
         email TEXT NOT NULL,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        birthday INTEGER NOT NULL,
+        age INTEGER NOT NULL,
+        gender TEXT NOT NULL
     )
     """
 )
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, make_response, jsonify, redirect, abort
 import sqlite3 as sql
+import time
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER'] = os.getcwd()+'/media'
 # print(app.config['UPLOAD_FOLDER'])
@@ -22,20 +23,32 @@ def create():
     if request.method == 'POST':
         con = sql.connect('database.db')
         try:
+            last_name = request.form['last_name']
+            first_name = request.form['first_name']
             email = request.form['email']
             password = request.form['password']
+            birthday_year = request.form['birthday_year']
+            birthday_month = request.form['birthday_month']
+            birthday_day = request.form['birthday_day']
+            gender = request.form['gender']
+            
+            name = last_name + first_name
+            birthday = birthday_year + '/' + birthday_month + '/' + birthday_day
+
+            now = time.strftime("%Y,%m,%d")
+            t = now.split(',')
+
+            if int(t[1]) >= int(birthday_month):
+                if int(t[2]) >= int(birthday_day):
+                    age = int(t[0]) - int(birthday_year)
+            else:
+                age = int(t[0]) - int(birthday_year) - 1
 
             # todo 驗證是否有此Email
-            email = email if len(email) else None
-            if email is None:
-                raise Exception('帳號不可為空')
 
             # todo 密碼需有大寫字母 字元不可重複3次
-            password = password if len(password) else None
-            if password is None:
-                raise Exception('密碼不可為空')
 
-            con.execute('INSERT INTO account(email, password) VALUES(?, ?)', (email, password))
+            con.execute('INSERT INTO member(name, email, password, birthday, age, gender) VALUES(?, ?, ?, ?, ?, ?)', (name, email, password, birthday, age, gender))
             con.commit()
             print('新增成功')
         except Exception as e:
@@ -44,7 +57,7 @@ def create():
             con.rollback()
         finally:
             con.close()
-            return render_template('log-in.html')
+            return redirect(url_for('index'))
 
 @app.route('/jp')
 def jp():
