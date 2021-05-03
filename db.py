6a528cd0bf44a1cfb3da807acf70f08f78652539
FinallyPeace <ll7856l@gmail.com>
2021-05-03 23:16:29 +0800

    註冊->資料庫->登入->首頁

--- db.py
+++ db.py
@@ -1,19 +1,98 @@
 import sqlite3 as sql
-con = sql.connect('database.db')
-print('Opened database successfully')
 
-con.execute(
-    """
-    CREATE TABLE member (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL,
-        password TEXT NOT NULL,
-        birthday INTEGER NOT NULL,
-        age INTEGER NOT NULL,
-        gender TEXT NOT NULL
-    )
-    """
-)
-print('Table created successfully')
-con.close()
+class DatebaseDriver(object):
+    def __init__(self):
+        # Sharing a single SQLite connection in multiple threads 
+        self.con = sql.connect('database.db', check_same_thread = False)
+        print('Opened database successfully')
+        self.create_table()
+    
+    def create_table(self):
+        try:
+            self.con.execute(
+                """
+                CREATE TABLE member (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    email TEXT NOT NULL,
+                    password TEXT NOT NULL,
+                    birthday INTEGER NOT NULL,
+                    age INTEGER NOT NULL,
+                    gender TEXT NOT NULL
+                );
+                """
+            )
+            print('Table created successfully')
+        except Exception as e:
+            print(e)
+
+    def get_all_members(self):
+        cursor = self.con.execute(
+            "SELECT * FROM member;"
+        )
+        members = []
+        for row in cursor:
+            members.append({
+                "id": row[0],
+                "name": row[1],
+                "email": row[2],
+                "password": row[3],
+                "birthday": row[4],
+                "age": row[5],
+                "gender": row[6]
+            })
+        return members
+
+    def insert_member_table(self, name, email, password, birthday, age, gender):
+        cursor = self.con.execute(
+            """
+            INSERT INTO member(name, email, password, birthday, age, gender) 
+            VALUES(?, ?, ?, ?, ?, ?);
+            """, (name, email, password, birthday, age, gender))
+        self.con.commit()
+        # 回傳最後一筆id
+        return cursor.lastrowid
+
+    def get_member_by_id(self, id):
+        cursor = self.con.execute(
+            """
+            SELECT * FROM member 
+            WHERE id = ?;
+            """,(id,)
+        )
+        for row in cursor:
+            return row
+        return None
+
+    def is_member(self, email):
+        cursor = self.con.execute(
+            """
+            SELECT * FROM member 
+            WHERE email = ?;
+            """,(email,)
+        )
+        for row in cursor:
+            return True
+        return False
+
+    def get_member_name_by_login(self, email, password):
+        cursor = self.con.execute(
+            """
+            SELECT name FROM member 
+            WHERE email = ? AND password = ?;
+            """,(email, password)
+        )
+        for row in cursor:
+            return row[0]
+        return None
+
+    # 改密碼
+    def update_member_password_by_login(self, new_password, email, old_password):
+        cursor = self.con.execute(
+            """
+            UPDATE member 
+            SET password = ?
+            WHERE email = ? AND password = ?;
+            """,(new_password, email, old_password)
+        )
+        self.con.commit()
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, url_for, make_response, jsonify, redirect, abort
 import sqlite3 as sql
+import json
 import time
+# db.py
+import db
+
+DB = db.DatebaseDriver()
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = os.getcwd()+'/media'
-# print(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
 def index():
@@ -11,51 +14,66 @@ def index():
 
 @app.route('/log-in')
 def log_in():
+    # todo 已登入後要顯示會員基本資料
     return render_template('log-in.html')
 
-# todo 驗證帳號密碼是否在資料庫中
 @app.route('/verify', methods=['POST'])
 def verify():
-    return redirect(url_for('index'))
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+    # todo 驗證帳密是否含有攻擊字元再執行SQL指令
+    # Ex: "' or '1' = '1'"
+
+    member_name = DB.get_member_name_by_login(email, password)
+    if member_name is None:
+        print("帳號密碼有誤，請重新輸入")
+    else:
+        print(f"{member_name} 歡迎回來")
+        return redirect(url_for('index'))
+
+    return redirect(url_for('log_in'))
 
 @app.route('/create', methods=['POST'])
 def create():
     if request.method == 'POST':
-        con = sql.connect('database.db')
-        try:
-            last_name = request.form['last_name']
-            first_name = request.form['first_name']
-            email = request.form['email']
-            password = request.form['password']
-            birthday_year = request.form['birthday_year']
-            birthday_month = request.form['birthday_month']
-            birthday_day = request.form['birthday_day']
-            gender = request.form['gender']
+        last_name = request.form['last_name']
+        first_name = request.form['first_name']
+        email = request.form['email']
+        password = request.form['password']
+        birthday_year = request.form['birthday_year']
+        birthday_month = request.form['birthday_month']
+        birthday_day = request.form['birthday_day']
+        gender = request.form['gender']
+
+        name = last_name + first_name
+        birthday = birthday_year + '/' + birthday_month + '/' + birthday_day
+
+        now = time.strftime("%Y,%m,%d")
+        t = now.split(',')
+        if int(t[1]) >= int(birthday_month):
+            if int(t[2]) >= int(birthday_day):
+                age = int(t[0]) - int(birthday_year)
+        else:
+            age = int(t[0]) - int(birthday_year) - 1
+
+        # todo 驗證表單資料是否正確再放入資料庫
+        # todo 驗證帳密是否含有攻擊字元再執行SQL指令
+
+        ismember = DB.is_member(email)
+        if ismember:
+            print("此信箱已被註冊過")
+        else:
+            member_id = DB.insert_member_table(name, email, password, birthday, age, gender)
+
+            member = DB.get_member_by_id(member_id)
+            member_name = DB.get_member_name_by_login(email, password)
+            if member is None:
+                print("註冊失敗")
             
-            name = last_name + first_name
-            birthday = birthday_year + '/' + birthday_month + '/' + birthday_day
-
-            now = time.strftime("%Y,%m,%d")
-            t = now.split(',')
-
-            if int(t[1]) >= int(birthday_month):
-                if int(t[2]) >= int(birthday_day):
-                    age = int(t[0]) - int(birthday_year)
-            else:
-                age = int(t[0]) - int(birthday_year) - 1
-
-            # todo 驗證表單資料是否正確再放入資料庫
-
-            con.execute('INSERT INTO member(name, email, password, birthday, age, gender) VALUES(?, ?, ?, ?, ?, ?)', (name, email, password, birthday, age, gender))
-            con.commit()
-            print('新增成功')
-        except Exception as e:
-            print('新增失敗')
-            app.logger.error(e)
-            con.rollback()
-        finally:
-            con.close()
-            return redirect(url_for('log_in'))
+            print(f"{member_name} 您好! 很高興能為您服務~")
+        return redirect(url_for('log_in'))
 
 @app.route('/jp')
 def jp():
